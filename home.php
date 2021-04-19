4a86dbfb5a3467e6d31917229a8d3cc43e4f885b
hilmanyahya8 <hilmanyahya8@gmail.com>
2021-04-19 23:43:34 +0700

    project UTS

---
+++ home.php
@@ -0,0 +1,21 @@
+<div class="row" >
+  <div class="col-md-9" >
+    <div class="jumbotron" style="background-color: rgb(241 163 73);">
+      <h1 class="display-4">Selamat Datang di AyahCom</h1>
+      <p class="lead">Situs Website Jual Beli Komputer</p>
+      <hr class="my-4">
+      <p>Selamat Berkunjung</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Lebih Lanjut</a>
+  </div>
+</div>
+
+<div class="col-md-3">
+  <ul class="list-group">
+    <li class="list-group-item active" aria-current="true">Produk</li>
+    <li class="list-group-item">LENOVO</li>
+    <li class="list-group-item">ASUS</li>
+    <li class="list-group-item">APPLE</li>
+    <li class="list-group-item">ACER</li>
+  </ul>
+</div>
+</div>
---
+++ sidebar.php
@@ -0,0 +1,13 @@
+
+<div class="col-md-4">
+	<!-------awal sidebar----------->
+<ul class="list-group">
+  <li class="list-group-item active" aria-current="true">Produk</li>
+  <li class="list-group-item">LENOVO</li>
+  <li class="list-group-item">ASUS</li>
+  <li class="list-group-item">APPLE</li>
+  <li class="list-group-item">ACER</li>
+</ul>
+    <!-------akhir sidebar----------->
+</div>
+    
